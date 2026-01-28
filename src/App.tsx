36382fe4bd8fcc87e4897ec6f7c6f852de9daee1
realalabady <fakealabady+1@gmail.com>
2026-01-28 10:01:58 +0300

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { DottedSurface } from "./components/ui/dotted-surface";
 import { Footer } from "./components/ui/footer-section";
+import { NavBar } from "./components/ui/tubelight-navbar";
+import { Briefcase, FileText, Home, User } from "lucide-react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const navItems = [
+    { name: "Home", url: "#", icon: Home },
+    { name: "About", url: "#", icon: User },
+    { name: "Projects", url: "#", icon: Briefcase },
+    { name: "Resume", url: "#", icon: FileText },
+  ];
+
   return (
     <>
       <DottedSurface />
+      <NavBar items={navItems} />
       <div className="app-content relative flex min-h-svh flex-col">
         <main className="flex flex-1 flex-col items-center justify-center gap-6 px-6 text-center">
           <div>
